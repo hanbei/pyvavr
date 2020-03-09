f36599af53a25b9a65981430aee747da5b6e8d2a
Florian Schulz <florian.schulz@convit.de>
2020-03-09 16:23:05 +0100

    Try

---
+++ pyvavr/attempts.py
@@ -0,0 +1,98 @@
+from abc import abstractmethod, ABC
+from typing import TypeVar, Generic, Callable, Union
+
+from pyvavr import ValueException
+
+R = TypeVar("R")
+T = TypeVar("T")
+E = TypeVar("E")
+
+
+class Try(ABC, Generic[T, E]):
+
+    def __init__(self, function: Callable[..., R], *args, **kwargs):
+        try:
+            self._result = function(args, kwargs)
+        except Exception as e:
+            self._exception = e
+
+    @classmethod
+    def success(cls, result):
+        return Success(result)
+
+    @classmethod
+    def failure(cls, exception):
+        return Failure(exception)
+
+    @abstractmethod
+    def map(self, function: Callable[[T], R]) -> 'Try[R]':
+        pass
+
+    @abstractmethod
+    def get(self) -> T:
+        pass
+
+    @abstractmethod
+    def get_error(self) -> E:
+        pass
+
+    @abstractmethod
+    def or_else(self, alternative: Union[T, Callable[[], T]]) -> T:
+        pass
+
+    @abstractmethod
+    def is_success(self) -> bool:
+        pass
+
+    def is_failure(self) -> bool:
+        return not self.is_success()
+
+
+class Success(Try):
+
+    def __init__(self, result):
+        super().__init__(lambda x: result)
+        self._result = result
+
+    def is_success(self) -> bool:
+        return True
+
+    def get(self) -> T:
+        return self._result
+
+    def get_error(self) -> E:
+        raise ValueException("cause not supported on success")
+
+    def or_else(self, alternative: Union[T, Callable[[], T]]) -> T:
+        return self._result
+
+    def map(self, function: Callable[[T], R]) -> 'Try[R]':
+        try:
+            return Success(function(self._result))
+        except Exception as e:
+            return Failure(e)
+
+
+class Failure(Try):
+
+    def __init__(self, exception: Exception):
+        super().__init__(lambda x: None)
+        self._exception = exception
+
+    def is_success(self) -> bool:
+        return False
+
+    def get(self) -> T:
+        raise self._exception
+
+    def get_error(self) -> E:
+        return self._exception
+
+    def map(self, function: Callable[[T], R]) -> 'Try[R]':
+        return self
+
+    def or_else(self, alternative: Union[T, Callable[[], T]]) -> T:
+        if callable(alternative):
+            return alternative()
+        else:
+            return alternative
---
+++ tests/try_test.py
@@ -0,0 +1,77 @@
+import pytest
+
+from pyvavr import ValueException
+from pyvavr.attempts import Try
+
+
+@pytest.fixture
+def failed_try():
+    return Try.failure(Exception("Some error"))
+
+
+@pytest.fixture
+def successful_try():
+    return Try.success("Some")
+
+
+def test_failure_is_failure(failed_try):
+    assert failed_try.is_failure() == True
+
+
+def test_success_is_not_failure(successful_try):
+    assert successful_try.is_failure() == False
+
+
+def test_map_success(successful_try):
+    assert successful_try.map(lambda x: x + " Bla").get() == "Some Bla"
+
+
+def failing_map(x):
+    raise NotImplementedError
+
+
+def test_map_fails_is_failure(successful_try):
+    try_map = successful_try.map(failing_map)
+    assert try_map.is_failure() == True
+    assert isinstance(try_map.get_error(), NotImplementedError)
+
+
+def test_map_failure(failed_try):
+    try_map = failed_try.map(lambda x: x + " Bla")
+    assert try_map.or_else("Test") == "Test"
+    assert try_map.is_failure() == True
+
+
+def test_get_on_success_returns_value(successful_try):
+    assert successful_try.get() == "Some"
+
+
+def test_get_on_failure_returns_value(failed_try):
+    with pytest.raises(Exception):
+        var = failed_try.get()
+
+
+def test_get_error_on_failure(failed_try):
+    error = failed_try.get_error()
+    assert isinstance(error, Exception)
+
+
+def test_get_error_on_success(successful_try):
+    with pytest.raises(ValueException):
+        var = successful_try.get_error()
+
+
+def test_or_else_failed(failed_try):
+    assert failed_try.or_else("Other") == "Other"
+
+
+def test_or_else_success(successful_try):
+    assert successful_try.or_else("Other") == "Some"
+
+
+def test_success_is_success(successful_try):
+    assert successful_try.is_success() == True
+
+
+def test_failure_is_not_success(failed_try):
+    assert failed_try.is_success() == False
